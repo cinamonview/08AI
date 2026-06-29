bfba97134e3b85623dbbbd03a4844b1a15c8d83a
cinamonview <cinamonview@gmail.com>
2026-06-29 18:43:00 +0900

    2026 0629 싱크체인지

---
+++ 02MachineLearn/02xor-train2.py
@@ -0,0 +1,39 @@
+
+# 판다스 모듈 임포르트
+import pandas as pd
+from sklearn import svm, metrics
+
+
+# XOR 의 계산 결과 데이터
+# XOR 논리 데이터
+xor_input = [
+    [0,0,0],
+    [0,1,1],
+    [1,0,1],
+    [1,1,0],
+]
+
+# 학습 전용 데이터와 테스트 전용 데이터로 분류
+
+# 데이터를 데이터프레임으로 변환
+xor_df = pd.DataFrame(xor_input)
+# 학습 전용 데이터와 테스트(레이블) 전용 데이터로 분류
+xor_data = xor_df.loc[:,0:1]  # 데이터
+xor_label = xor_df.loc[:,2] # 레이블
+print('xor_data\n',xor_data)
+
+# 데이터 학습 및 예측
+
+clf = svm.SVC()
+clf.fit(xor_data, xor_label)
+pre = clf.predict(xor_data)
+
+
+# 정답률 구하기
+ac_score = metrics.accuracy_score(xor_label,pre)
+print(" 정답률 = ", ac_score)
+
+'''
+싸이킷런에서 제공하는 내장함수를 통해 학습용 프로그램을
+간단히 작성할 수 있다.
+'''
---
+++ 02MachineLearn/03iris-train1.py
@@ -0,0 +1,85 @@
+
+
+
+from sklearn import svm,metrics
+import random, re
+
+
+# 붗꽃의 CSV 데이터 읽어들이기
+# 데이터 저장을 위한 리스트를 새로 생성한다.
+csv = []
+# csv comma separate 어쩌구
+# csv 파일을 읽기 (r) 모드로 오픈한다.
+with open('./resData/iris.csv', 'r',encoding='utf-8') as fp:
+    # 파일의 내용을 한줄씩 읽기
+    for line in fp:
+        # 줄바꿈 제거를 해준다.
+        line = line.strip()
+        # 컴마를 기준으로 문자열을 분리해서 배열로 전환해준다.
+        cols = line.split(',')
+        '''
+        문자열 데이터를 실수로 변환하는 기능으로
+        람다식 정의 
+        csv 파일에서 읽어온 데이터는 문자이므로 실수로 변환이 필요하다.
+        정규 표현식을 통해 정의함
+            ^ : 문자열의 시작
+            [0.9\.] : 숫자(0~9) 또는 마침표(.)가 한번이상 나타나는 패턴을 의미한다.
+            $ : 문자열의 끝
+            따라서 123 혹은 12.34와 같은 문자와 매칭된다.
+        '''
+        # 이 람다식의 내용은 
+        # 정규표현식과 매칭되면 float를 통해  실수로 변환하겠다는 의미.
+        # 즉 훈련데이터는 실수로, 레이블 데이터는 문자 그대로 사용한다.
+        fn = lambda n : float(n) if re.match(r'^[0-9\.]+$',n) else n
+        # cols의 크기만큼 반복해서 fn을 호출한 결과로 리스트를 생성한다.
+        cols = list(map(fn, cols))
+        # 이렇게 만들어진 결과를 scv 리스트에 추가한다.
+        csv.append(cols)
+
+# 첫번째 줄의 헤더는 제거한다.(컬럼명이 입력되어 있다.)
+del csv[0]
+# 데이터 셔플 . 즉 전체적으로 섞어준다.
+random.shuffle(csv)
+# 전체 데이터의 갯수 확인.
+total_len = len(csv)
+print("데이터 갯수", total_len)
+
+# 학습데이터 (100개)와 테스트데이터 (50개) 분할하기 (2:1 비율)
+
+'''
+데이터를 이와같이 나누는 이유는 훈련에 사용하지 않은
+데이터를 테스트에 활용해야 학습이 제대로 되었는지 확인할 수 있기 때문이다.
+'''
+
+train_len = int(total_len*2/3)
+# 훈련용 학습데이터 / 레이블데이터를 저장하기 위한 리스트
+train_data = []
+train_label = []
+# 테스트용 학습데이터 /레이블데이터
+test_data = []
+test_label = []
+
+# 전체 데이터 갯수만큼 반복
+for i in range(total_len):
+    #학습 및 레이블 데이터로 분리
+    # 데이터와 라벨로 분리
+    data = csv[i][0:4] # 0~3열까지
+    label = csv[i][4] # 4열
+    if i < train_len:
+        # 훈련용 데이터 추가 : 100개
+        train_data.append(data)
+        train_label.append(label)
+    else :
+        # 테스트 데이터 추가 : 50개
+        test_data.append(data)
+        test_label.append(label)
+        
+# 데이터를 학습시키고 예측하기
+
+clf = svm.SVC()
+clf.fit(train_data,train_label)
+pre = clf.predict(test_data)
+
+#정답률 구하기
+ac_score = metrics.accuracy_score(test_label,pre)
+print(" 정답률 = ", ac_score)
